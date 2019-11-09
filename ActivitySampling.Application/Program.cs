6e55b5d9c2a47e3e51a39c5142829ab02790f7b2
Christof Konstantinopoulos <christof@konstantinopoulos.de>
2019-11-09 04:03:35 +0100

    RunLogic Task refactored

--- ActivitySampling.Application/Program.cs
+++ ActivitySampling.Application/Program.cs
@@ -10,6 +10,8 @@ namespace ActivitySampling.Application
 {
     class Program
     {
+        private static readonly TimeSpan IntervalWithoutCommandLineArg = TimeSpan.FromSeconds(30);
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -23,16 +25,7 @@ namespace ActivitySampling.Application
 
         static async Task RunLogic(string[] args)
         {
-            TimeSpan Interval = TimeSpan.FromSeconds(30);
-
-            if (args.GetUpperBound(0) >= 0)
-            {
-                var ok = int.TryParse(args[0], out int min);
-                if (ok)
-                {
-                    Interval = TimeSpan.FromMinutes(min);
-                }
-            }
+            TimeSpan Interval = GetIntervalFromCommandLineArgs(args);
 
             var bl = new BusinessLogic();
             await bl.Run(Interval);
@@ -41,7 +34,21 @@ namespace ActivitySampling.Application
             {
                 Thread.Sleep(1000);
             }
+        }
 
+        private static TimeSpan GetIntervalFromCommandLineArgs(string[] args)
+        {
+            TimeSpan result = IntervalWithoutCommandLineArg;
+
+            if (args.GetUpperBound(0) >= 0)
+            {
+                var ok = int.TryParse(args[0], out int minutes);
+                if (ok)
+                {
+                    result = TimeSpan.FromMinutes(minutes);
+                }
+            } 
+            return result;
         }
     }
 }
