12d029a2b9190d0834da44292a401cb94d6e6324
Christof <chrkon@web.de>
2019-12-29 23:36:49 +0100

    auto generated tests added
    xUnit Tests for the Module Classes generated by Visual Studio "Create Unit Tests"

---
+++ ActivitySampling.Tests/Module_CommandLineInterfaceTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using ActivitySampling.Module.View.CLI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivitySampling.Module.View.CLI.Tests
+{
+    public class Module_CommandLineInterfaceTests
+    {
+        [Fact()]
+        public void ReadKey_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ReadLine_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void WriteLine_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ShowLastQuestion_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ShowQuestion_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
---
+++ ActivitySampling.Tests/Module_ViewCLITests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using ActivitySampling.Module.View.CLI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivitySampling.Module.View.CLI.Tests
+{
+    public class Module_ViewCLITests
+    {
+        [Fact()]
+        public void ViewCLI_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AskForActivity_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void EditLastActivity_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ActivateMenu_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DeactivateMenu_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void Output_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
