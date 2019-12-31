dc2ec3c70a34c341995a4b40160d980555d665a2
Christof <chrkon@web.de>
2019-12-31 13:54:57 +0100

    Create CommandLineInterfaceFakeTests.cs

---
+++ ActivitySampling.Tests/Fakes/CommandLineInterfaceFakeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace ActivitySampling.Tests
+{
+    public class CommandLineInterfaceFakeTests
+    {
+        [Fact()]
+        public void ReadKey_W_sucessful_Test()
+        {
+            var expected = ConsoleKey.W;
+            var sut = new CommandLineInterfaceFake();
+            sut.NextInputKey = expected;
+            bool isKeyAvailable = sut.KeyAvailable;
+            Assert.True(isKeyAvailable);
+            ConsoleKeyInfo result = sut.ReadKey(false);
+            Assert.True(result.Key == expected, "unexpected result");
+        }
+
+        [Fact()]
+        public void ReadKey_NoKey_Test()
+        {
+            var sut = new CommandLineInterfaceFake();
+            bool isKeyAvailable = sut.KeyAvailable;
+            Assert.False(isKeyAvailable);
+            ConsoleKeyInfo result = sut.ReadKey(false);
+            Assert.True(result.Key == 0);
+        }
+
+    }
+}
